1394f99cab1c641745aed357e840abfd9500484b
RohanRiders <159487815+RohanRiders@users.noreply.github.com>
2024-05-02 11:30:40 -0600

    Basic console apps

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("I was walking with her at the park and she said," +
+            " \" I can't believe this is happening. \" isn't that interesting");
+        Console.WriteLine("Welcome to Acme Accouting Systems\nRemember, we're \"accounting\" on you!");
+        Console.ReadLine();
+        }
+    }
+

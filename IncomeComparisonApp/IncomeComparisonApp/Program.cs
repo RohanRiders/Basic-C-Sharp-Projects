a4d96d270e7395b17218db8a9650627ad159e5cc
RohanRiders <159487815+RohanRiders@users.noreply.github.com>
2024-05-03 10:12:23 -0600

    Annual Salary Calculation Console App with some additional Math calculation practice.

---
+++ IncomeComparisonApp/IncomeComparisonApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeComparisonApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //This is the initial message at the beginning of the program.
+            string initialMessage = "Anonymous Income Comparison Program";
+            Console.WriteLine(initialMessage);
+            
+
+            //Create "Person 1" profile with hourly rate and hours worked per week.
+            string person1 = "Person 1:";
+            Console.WriteLine(person1);
+
+            string hourlyRateP1 = "What is your hourly rate?";
+            Console.WriteLine(hourlyRateP1);
+            string currentHourlyRateP1 = Console.ReadLine();
+
+
+            string hoursPerWeekP1 = "What is your hours worked per week?";
+            Console.WriteLine(hoursPerWeekP1);
+            string currentWeeklyHoursP1 = Console.ReadLine();
+
+            //Calculating Person 1 rate and printing the results. 
+            int currentHourlyRateP1number = Convert.ToInt32(currentHourlyRateP1);
+            int currentWeeklyHoursP1number = Convert.ToInt32(currentWeeklyHoursP1);
+            int weeksInYearP1 = 52;
+            int annualSaleryP1 = (currentHourlyRateP1number * currentWeeklyHoursP1number) * weeksInYearP1;
+            Console.WriteLine("This is person 1's annual salary: " + annualSaleryP1);
+
+
+            //Create "Person 2" profile with hourly rate and hours worked per week. 
+            string person2 = "Person 2:";
+            Console.WriteLine(person2);
+
+            string hourlyRateP2 = "What is your hourly rate?";
+            Console.WriteLine(hourlyRateP2);
+            string currentHourlyRateP2 = Console.ReadLine();
+
+
+            string hoursPerWeekP2 = "What is your hours worked per week?";
+            Console.WriteLine(hoursPerWeekP2);
+            string currentWeeklyHoursP2 = Console.ReadLine();
+            
+
+            //Calculating Person 2 rate and printing the results. 
+            int currentHourlyRateP2number = Convert.ToInt32(currentHourlyRateP2);
+            int currentWeeklyHoursP2number = Convert.ToInt32(currentWeeklyHoursP2);
+            int weeksInYearP2 = 52;
+            int annualSaleryP2 = (currentHourlyRateP2number * currentWeeklyHoursP2number) * weeksInYearP2;
+            Console.WriteLine("This is person 2's annual salary: " + annualSaleryP2);
+
+
+            bool whoIsRich = (annualSaleryP1 > annualSaleryP2);
+            Console.WriteLine("Does Person 1 make more money than Person 2: " + whoIsRich);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ MathCalculationApp/MathCalculationApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCalculationApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //This will take in a number from the user and print it to the console. 
+            //string userRequest = "Give me any number and then we will multiply by 50";
+            //Console.WriteLine(userRequest);
+            //string userNumberInput = Console.ReadLine();
+            //int num1 = Convert.ToInt32(userNumberInput);
+            //int total = num1 * 50;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //string businessRequest = "Please give me a number, any number, and I will add 25 to it and give you the result.";
+            //Console.WriteLine(businessRequest);
+            //string userInput = Console.ReadLine();
+            //int total = Convert.ToInt32(userInput);
+            //int result = total + 25;
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+
+        }
+    }
+}

a0a7ed83c8c35a1dda9d87bdf781779605c6e772
RohanRiders <159487815+RohanRiders@users.noreply.github.com>
2024-05-15 13:49:50 -0600

    The initial work to create the game TwentyOne

---
+++ TwentyOne/TwentyOne/Deck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Deck
+    {
+        public Deck() //When this constructor is called this is the behavior that takes place by default as soon as an object is created. 
+        {
+
+            //What we know is each deck has 52 cards, 13 faces with 4 suites. So for each face (13) we need to loop through it 4 times. 
+            Cards = new List<Card>(); //An empty list of cards
+            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" }; 
+            List<string> Faces = new List<string>()
+            {
+                "Two", "Three", "Four", "Five", "Six", "Seven",
+                "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
+            };
+
+            foreach (string face in Faces) //Every iteration of faces it will loop through all the suits for that face. 
+            {
+                foreach (string suit in Suits) //During each loop we create a card and assign the values of suit and face to that card. 
+                {
+                    Card card = new Card();// This only exists with each loop 
+                    card.Suit = suit;
+                    card.Face = face;
+                    Cards.Add(card);
+                }
+            }
+
+
+            //Cards = new List<Card>();
+            //Card cardOne = new Card();
+            //cardOne.Face = "Two";
+            //cardOne.Suit = "Hearts";
+            //Cards.Add(cardOne);
+        }
+        public List<Card> Cards { get; set; }
+
+    }
+}
---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck(); //Upon instantiating a new object of object/Class Deck it should have created 52 cards for you. This object has a property "Cards" with data type list that has 52 cards in it. 
+            deck = Shuffle(deck); //This looks weird but we are taking in the variable deck, shuffling it, and reasigning it back to the veriable 'deck'. 
+            // We want a function that takes in a deck of cards and returns a shuffled deck of cards. 
+
+            foreach (Card card in deck.Cards) //The Cards list should have 52 card objects in it and we want to print them to the screen. 
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count); //Give me the total number of cards in the list.
+            Console.ReadLine();
+
+            //Console.WriteLine(deck.Cards[0].Face + " of " + deck.Cards[0].Suit); //Zero based indexing tells us that one card was added to the list. What if you added a foreach loop. 
+            //Console.WriteLine(cardOne.Face + " of " + cardOne.Suit);
+
+
+            /// ********************************************************************************** NOTES *******************************************************
+            //deck.Cards = new List<Card>(); //You have to instantiate Cards which is of data type list, so you need to create a new list in order to use it. 
+            //Card cardOne = new Card(); //This is an object with properties of Face and Suit.
+            //cardOne.Face = "Queen";
+            //cardOne.Suit = "Spades";
+            //deck.Cards.Add(cardOne);
+        }
+        public static Deck Shuffle(Deck deck)
+        {
+            // We want to shuffle to make it random. 
+            List<Card> TempList = new List<Card>(); 
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomindex = random.Next(0, deck.Cards.Count);
+                TempList.Add(deck.Cards[randomindex]);
+                deck.Cards.RemoveAt(randomindex);
+            }
+            deck.Cards = TempList;
+            return deck;
+
+        }
+    }
+}

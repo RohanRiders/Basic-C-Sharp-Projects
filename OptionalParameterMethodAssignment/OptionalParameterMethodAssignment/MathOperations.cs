c7de40bc94afeb27848bb1c52a1e772bafc1bd81
RohanRiders <159487815+RohanRiders@users.noreply.github.com>
2024-05-16 11:23:35 -0600

    Adding my notes to a Console App made for the practicing of Optional Parameters.

---
+++ OptionalParameterMethodAssignment/OptionalParameterMethodAssignment/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterMethodAssignment
+{
+    class MathOperations
+    {
+        public int MultiplyMe(int num1 , int num2 = 5)
+        {
+            int product = num1 * num2;
+            return product;
+
+        }
+
+
+    }
+}
---
+++ OptionalParameterMethodAssignment/OptionalParameterMethodAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations Multiply = new MathOperations();
+
+            Console.WriteLine("Give me a number?");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            int result = Multiply.MultiplyMe(userNum1);
+            Console.WriteLine(result);
+
+            //Console.WriteLine("give me a number");
+            //int userNum1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("give me another number, but you don't have to because it is an optional parameter");
+            //int userNum2 = Convert.ToInt32(Console.ReadLine());
+            
+            //if (userNum2 > 0)
+            //{
+            //    int finalresult = Multiply.MultiplyMe(userNum1);
+            //    Console.WriteLine(finalresult);
+                
+            //}
+            //else
+            //{
+            //    int finalresult = Multiply.MultiplyMe(userNum1, userNum2);
+            //    Console.WriteLine(finalresult);
+            //}
+            Console.ReadLine(); 
+        }
+    }
+}
